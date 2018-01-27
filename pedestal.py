293ebd020e57d08c302b1f49e18341a2c5fb4f2c
Sidicley <sidicleyribeiro@gmail.com>
2018-01-27 19:36:26 -0300

    "Important functions to pedestal.py"

--- pedestal.py
+++ pedestal.py
@@ -5,28 +5,27 @@ import meshio
 class MeshHandler:
 
     def __init__(self, mesh_file = None):
-        points, cells, point_data, cell_data, field_data = \
+        self.points, self.cells, self.point_data, self.cell_data, self.field_data =
             meshio.read(mesh_file)
         self.mesh_data = h5py.File("new_mesh.hdf5", 'w')
 
         self.all_entities = self.mesh_data.create_group('all_entities')
-        node_data = self.all_entities.create_group('node_data')
-        line_data = self.all_entities.create_group('line_data')
-        cell_2D_data = self.all_entities.create_group('cell_2D_data')
-        cell_3D_data = self.all_entities.create_group('cell_3D_data')
+        self.node_data = self.all_entities.create_group('node_data')
+        self.line_data = self.all_entities.create_group('line_data')
+        self.cell_2D_data = self.all_entities.create_group('cell_2D_data')
+        self.cell_3D_data = self.all_entities.create_group('cell_3D_data')
 
-        field_h5dir = self.mesh_data.create_group('fields')
+        self.field_h5dir = self.mesh_data.create_group('fields')
 
     def store_line_connectivities(self):
         lines_connectivities = self.cells['line']
-
+        pass
 
 
     def get_entities_by_dimension(self, dimension):
         if dimension == 0:
             return range(len(self.points))
         if dimension == 1:
-
             pass
         if dimension == 2:
             pass
@@ -34,10 +33,18 @@ class MeshHandler:
             pass
 
     def get_adjacencies(self, entity):
+        pass
 
+    def get_bridge_adjacencies(self, entity):
 
+    def get_entities_by_dimension(self, dim):
+        pass
 
+    def create_entities(self, entities):
+        pass
 
+    def delete_entities(self, entity):
+        pass
 
-
-    def get_entities_by_dimension(self, )
+    def create_meshseet(self):
+        pass
