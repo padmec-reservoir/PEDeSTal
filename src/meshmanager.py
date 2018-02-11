17c375c1512775ee1e05b5b3041a3ed09714a909
Sidicley <sidicleyribeiro@gmail.com>
2018-02-11 00:10:34 -0300

    Adding hdf5 initialization test-refactoring

--- src/meshmanager.py
+++ src/meshmanager.py
@@ -1,50 +1,18 @@
 import h5py
 import meshio
 
+class MeshManager:
 
-class MeshManeger:
-
-    def __init__(self, mesh_file = None):
-        self.points, self.cells, self.point_data, self.cell_data, self.field_data =
-            meshio.read(mesh_file)
-        self.mesh_data = h5py.File("new_mesh.hdf5", 'w')
-
-        self.all_entities = self.mesh_data.create_group('all_entities')
-        self.node_data = self.all_entities.create_group('node_data')
-        self.line_data = self.all_entities.create_group('line_data')
-        self.cell_2D_data = self.all_entities.create_group('cell_2D_data')
-        self.cell_3D_data = self.all_entities.create_group('cell_3D_data')
-
-        self.field_h5dir = self.mesh_data.create_group('fields')
-
-    def store_line_connectivities(self):
-        lines_connectivities = self.cells['line']
-        pass
-
-
-    def get_entities_by_dimension(self, dimension):
-        if dimension == 0:
-            return range(len(self.points))
-        if dimension == 1:
-            pass
-        if dimension == 2:
-            pass
-        if dimension == 3:
-            pass
-
-    def get_adjacencies(self, entity):
-        pass
-
-    def get_bridge_adjacencies(self, entity):
-
-    def get_entities_by_dimension(self, dim):
-        pass
-
-    def create_entities(self, entities):
-        pass
-
-    def delete_entities(self, entity):
-        pass
-
-    def create_meshseet(self):
-        pass
+    def __init__(self):
+        self.storage_file = h5py.File('storage.hdf5', 'w', driver='core', backing_store=False)
+        # self.points, self.cells, self.point_data, self.cell_data, self.field_data =
+        #     meshio.read(mesh_file)
+        # self.mesh_data = h5py.File("new_mesh.hdf5", 'w')
+        #
+        # self.all_entities = self.mesh_data.create_group('all_entities')
+        # self.node_data = self.all_entities.create_group('node_data')
+        # self.line_data = self.all_entities.create_group('line_data')
+        # self.cell_2D_data = self.all_entities.create_group('cell_2D_data')
+        # self.cell_3D_data = self.all_entities.create_group('cell_3D_data')
+        #
+        # self.field_h5dir = self.mesh_data.create_group('fields')
--- src/tests/test_meshmanager.py
+++ src/tests/test_meshmanager.py
@@ -1,7 +1,19 @@
 import unittest
+import h5py
 from ..meshmanager import MeshManager
 
 class MeshManagerTest(unittest.TestCase):
 
-    def test_file_loading(self):
-        pass
+    def setUp(self):
+        self.mesh = MeshManager()
+        self.file_instance = h5py.File('storage_test.hdf5', 'w', driver='core', backing_store=False)
+
+    def tearDown(self):
+        self.file_instance.close()
+
+    def test_hdf5_file_loading(self):
+        """
+        Test if HDF5 file has been loaded upon instanciation
+        Asserts with the name of the root dir of hdf5 file
+        """
+        self.assertEqual(self.mesh.storage_file.name, self.file_instance.name)
